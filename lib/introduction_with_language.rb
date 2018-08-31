3cf35f5998ef94804ca9118109c597531d089e31
Max Petersen <mglxp5@gmail.com>
2018-08-31 21:53:22 +0000

    automatically backed up by learn

--- lib/introduction_with_language.rb
+++ lib/introduction_with_language.rb
@@ -1,4 +1,3 @@
-#lib/introduction_with_language DO YOU KNOW WHAT TIME IT IS
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
